24877d0092b81af029a86054f54443a2e086a638
Thomas Cremel <thomas.cremel@epita.fr>
2025-09-29 10:46:03 +0200

    Change nix fmt from alejandra to nixfmt-tree

--- flake.nix
+++ flake.nix
@@ -3,27 +3,34 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
-  in {
-    packages = forAllSystems (system: pkgs: {
-      default = self.packages.${system}.zpotify;
-      zpotify = pkgs.callPackage ./nix/package.nix {
-        zig = pkgs.zig_0_15;
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (
+        system: pkgs: {
+          default = self.packages.${system}.zpotify;
+          zpotify = pkgs.callPackage ./nix/package.nix {
+            zig = pkgs.zig_0_15;
+          };
+        }
+      );
 
-    devShells = forAllSystems (system: pkgs: {
-      default = pkgs.callPackage ./nix/shell.nix {
-        zig = pkgs.zig_0_15;
-        zls = pkgs.zls_0_15;
-      };
-    });
+      devShells = forAllSystems (
+        system: pkgs: {
+          default = pkgs.callPackage ./nix/shell.nix {
+            zig = pkgs.zig_0_15;
+            zls = pkgs.zls_0_15;
+          };
+        }
+      );
 
-    formatter = forAllSystems (_: pkgs: pkgs.alejandra);
-  };
+      formatter = forAllSystems (_: pkgs: pkgs.nixfmt-tree);
+    };
 }
