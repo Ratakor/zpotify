64fd4de155400fce8c5c01780d63d05b3fd108c8
Ratakor <ratakor@disroot.org>
2025-09-29 10:46:03 +0200

    Change nix fmt from alejandra to nixfmt-tree

--- nix/package.nix
+++ nix/package.nix
@@ -9,57 +9,58 @@
   chafa,
   libjpeg,
   image-support ? true,
-}: let
+}:
+let
   fs = lib.fileset;
 in
-  stdenv.mkDerivation (finalAttrs: {
-    pname = "zpotify";
-    # Must match the `version` in `build.zig.zon`.
-    version = "0.4.0-dev";
+stdenv.mkDerivation (finalAttrs: {
+  pname = "zpotify";
+  # Must match the `version` in `build.zig.zon`.
+  version = "0.4.0-dev";
 
-    src = fs.toSource {
-      root = ../.;
-      fileset = fs.unions [
-        ../src
-        ../vendor
-        ../build.zig
-        ../build.zig.zon
-      ];
-    };
-
-    deps = callPackage ./deps.nix {};
-
-    zigBuildFlags = [
-      "--system"
-      "${finalAttrs.deps}"
-      # "--release=fast"
-      "-Dversion-string=${finalAttrs.version}"
-      "-Dimage-support=${lib.boolToString image-support}"
+  src = fs.toSource {
+    root = ../.;
+    fileset = fs.unions [
+      ../src
+      ../vendor
+      ../build.zig
+      ../build.zig.zon
     ];
+  };
 
-    nativeBuildInputs = [
-      installShellFiles
-      pkg-config
-      zig.hook
-    ];
+  deps = callPackage ./deps.nix { };
 
-    buildInputs = lib.optionals image-support [
-      glib # chafa dependency
-      chafa
-      libjpeg
-    ];
+  zigBuildFlags = [
+    "--system"
+    "${finalAttrs.deps}"
+    # "--release=fast"
+    "-Dversion-string=${finalAttrs.version}"
+    "-Dimage-support=${lib.boolToString image-support}"
+  ];
 
-    postInstall = ''
-      installShellCompletion --cmd zpotify \
-        --bash <($out/bin/zpotify completion bash) \
-        --zsh <($out/bin/zpotify completion zsh)
-    '';
+  nativeBuildInputs = [
+    installShellFiles
+    pkg-config
+    zig.hook
+  ];
 
-    meta = {
-      description = "CLI/TUI for Spotify";
-      homepage = "https://github.com/ratakor/zpotify";
-      license = lib.licenses.gpl3Only;
-      maintainers = [lib.maintainers.ratakor];
-      mainProgram = "zpotify";
-    };
-  })
+  buildInputs = lib.optionals image-support [
+    glib # chafa dependency
+    chafa
+    libjpeg
+  ];
+
+  postInstall = ''
+    installShellCompletion --cmd zpotify \
+      --bash <($out/bin/zpotify completion bash) \
+      --zsh <($out/bin/zpotify completion zsh)
+  '';
+
+  meta = {
+    description = "CLI/TUI for Spotify";
+    homepage = "https://github.com/ratakor/zpotify";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ lib.maintainers.ratakor ];
+    mainProgram = "zpotify";
+  };
+})
